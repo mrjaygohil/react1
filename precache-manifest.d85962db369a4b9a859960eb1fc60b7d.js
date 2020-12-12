6cc0a367afbc87e68a74b07bdba6d453d55aebbf
mr. Jay gohil <53848792+mrjaygohil@users.noreply.github.com>
2020-12-12 14:09:37 +0530

    Updates

---
+++ precache-manifest.d85962db369a4b9a859960eb1fc60b7d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "382fd958e98722f25b7041e343996de3",
+    "url": "/react1/index.html"
+  },
+  {
+    "revision": "1c5840bf0fcceed75783",
+    "url": "/react1/static/css/main.978d9712.chunk.css"
+  },
+  {
+    "revision": "0248216e3f6845cc8c9d",
+    "url": "/react1/static/js/2.3b6de89a.chunk.js"
+  },
+  {
+    "revision": "216acf140de0d4a41a4074f7de680b76",
+    "url": "/react1/static/js/2.3b6de89a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beb56ee62257c0ca3956",
+    "url": "/react1/static/js/3.0279cf92.chunk.js"
+  },
+  {
+    "revision": "1c5840bf0fcceed75783",
+    "url": "/react1/static/js/main.9254f0cb.chunk.js"
+  },
+  {
+    "revision": "dc192b57bfed2afd6be1ea5558f17255",
+    "url": "/react1/static/js/main.9254f0cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17f94c52631246656cb2",
+    "url": "/react1/static/js/runtime-main.bb8d3088.js"
+  },
+  {
+    "revision": "bd6820f1a4015e9645e470ea8e7b1501",
+    "url": "/react1/static/media/anime3.bd6820f1.png"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/react1/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/react1/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/react1/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/react1/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "db32ff0495a01a87b1de620c3252b83c",
+    "url": "/react1/static/media/star-sky.db32ff04.jpg"
+  }
+]);
